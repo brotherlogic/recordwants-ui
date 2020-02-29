3e7fcaac5e1f0124d96d223c42a78d2cae1131f6
brotherlogic <brotherlogic@gmail.com>
2020-02-29 11:29:18 -0800

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ export function provideConfig() {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
     SocialLoginModule,
     SigninComponent,
     FirstComponent
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
 
 @Component({
   selector: 'app-first',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+ 
+  private user: SocialUser;
+  private loggedIn: boolean;
+ 
+  constructor(private authService: AuthService) { }
+ 
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
   }
-
 }
