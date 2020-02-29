78c51994c56b5d0f430e78a95d1a0cad6d593286
Simon Tucker <brotherlogic@gmail.com>
2020-02-29 11:19:03 -0800

    Adds in some sign in stuffs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("Google-OAuth-Client-Id")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("Facebook-App-Id")
+  }
+]);
+ 
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
     AppComponent
+    SocialLoginModule,
+    SigninComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+  	     provide: AuthServiceConfig,
+	     useFactory: provideConfig
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
